21f7b096db87419005095cfa8b755909b322de27
mikeangelo007 <miguel.tovar@kleesoftware.com>
2020-06-24 22:58:48 -0500

    Eliminacion de archivos innecesarios

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/* const element = document.createElement('h1')
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+element.innerText='Hello Word Cruel:::'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const container= document.getElementById('root')
+
+container.appendChild(element) */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+//const element = <h1>Hello React...or</h1>
+//const name = 'Mike'
+const usuario = {
+  firstname: 'CHDLM',
+  lastname: '2.0',
+  avatar: 'https://www.show.news/__export/1567201752167/sites/debate/img/2019/08/30/gato_png_crop1567201738546.jpg_423682103.jpg'
+}
+
+function getName(user){
+  if(user){
+    return `${user.firstname} ${user.lastname}`
+  }
+  return 'Extraño ome'
+}
+
+function getGreeting(user){
+ if(user){
+ return <h1>Hola {getName(user)}</h1>
+ }
+return  <h2>Alerta {getName(user)}</h2>
+}
+
+
+//const element = <div>{getGreeting(usuario)}</div>
+
+//const element = <img src={usuario.avatar} />
+
+const element=(
+  <div>
+    {getGreeting(usuario)}
+    <img src={usuario.avatar} />
+  </div>
+)
+const container = document.getElementById('root')
+
+//ReactDOM.render(_QUE_,_DONDE_)
+ReactDOM.render(element,container)
